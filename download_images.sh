9387b32b65d01b53cc5079777b5288a2151d949e
Imene Kerboua <imenelydia.kr@gmail.com>
2024-07-19 13:58:47 +0200

    update ports number

--- download_images.sh
+++ download_images.sh
@@ -1,4 +1,5 @@
 wget http://metis.lti.cs.cmu.edu/webarena-images/shopping_final_0712.tar
+wget http://metis.lti.cs.cmu.edu/webarena-images/shopping_admin_final_0719.tar
 wget http://metis.lti.cs.cmu.edu/webarena-images/postmill-populated-exposed-withimg.tar
 wget http://metis.lti.cs.cmu.edu/webarena-images/gitlab-populated-final-port8023.tar
 wget http://metis.lti.cs.cmu.edu/webarena-images/wikipedia_en_all_maxi_2022-05.zim
